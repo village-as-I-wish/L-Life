ab710718134c8829004960d82e23e7d31e415d22
sskong777 <sskong777@gmail.com>
2023-08-16 17:23:04 +0900

    project setting

---
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/LLifeApplication.java
@@ -0,0 +1,13 @@
+package kosa.com.suntofu.L_LIFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LLifeApplication.class, args);
+	}
+
+}
---
+++ L_LIFE/src/test/java/kosa/com/suntofu/L_LIFE/LLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package kosa.com.suntofu.L_LIFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
