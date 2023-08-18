1fadc1f7a0da5b64931eb764604025b14cfca0a2
sskong777 <sskong777@gmail.com>
2023-08-18 17:40:28 +0900

    ëspring boot_oracle cloudã 연동

---
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/service/StandardService.java
@@ -0,0 +1,9 @@
+package kosa.com.suntofu.L_LIFE.standard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StandardService implements StandardServiceImpl{
+}
---
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/vo/StandardVO.java
@@ -0,0 +1,8 @@
+package kosa.com.suntofu.L_LIFE.standard.vo;
+
+import lombok.Data;
+
+@Data
+public class StandardVO {
+
+}
