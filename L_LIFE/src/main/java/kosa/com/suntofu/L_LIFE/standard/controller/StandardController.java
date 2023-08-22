8e9102c9a607505b86435b5e33fd3bd506ee6cc2
sskong777 <sskong777@gmail.com>
2023-08-22 09:28:02 +0900

    DB연동 테스트

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/controller/StandardController.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/controller/StandardController.java
@@ -1,11 +1,14 @@
 package kosa.com.suntofu.L_LIFE.standard.controller;
 
 import kosa.com.suntofu.L_LIFE.standard.service.StandardService;
+import kosa.com.suntofu.L_LIFE.standard.vo.StandardVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +19,13 @@ public class StandardController {
     @GetMapping
     public String loadStandardMainPage(Model model){
         model.addAttribute("data", "test");
+        return "livart_main";
+    }
+
+    @GetMapping(value="all")
+    public String showAllStandardFurniture(Model model){
+        List<StandardVO> standardList = standardService.getAllStandard();
+        System.out.println(standardList);
         return "livart_main";
     }
 }
--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/service/StandardServiceImpl.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/service/StandardServiceImpl.java
@@ -1,4 +1,10 @@
 package kosa.com.suntofu.L_LIFE.standard.service;
 
+import kosa.com.suntofu.L_LIFE.standard.vo.StandardVO;
+
+import java.util.List;
+
 public interface StandardServiceImpl {
+    List<StandardVO> getAllStandard();
+
 }
