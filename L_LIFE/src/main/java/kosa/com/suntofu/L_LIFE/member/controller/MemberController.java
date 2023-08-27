e5139cd877743b635a7ea0abccfa68b7c3d724c0
JOOWON <dev.joowon@gmail.com>
2023-08-27 21:03:53 +0900

    feat : [FE] 로그인 임시저장

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/member/controller/MemberController.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,9 +15,18 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/login")
-    public String loadLoginPage(){
-        return "pages/member/login";
+    public String loadLoginPage(){ return "pages/member/login"; }
+
+    //redirect 경로 mapping
+    @RequestMapping("/login/kakao")
+    public String kakaoLogin(@RequestParam(value = "code",required = false) String code){
+        if(code!=null){//카카오측에서 보내준 code가 있다면 출력합니다
+            System.out.println("여기 도착했습티");
+            System.out.println("code = " + code);
+        }
+        return null; //만들어둔 응답받을 View 페이지 redirectPage.html 리턴
     }
+
     @GetMapping("/mypage")
     public String loadMyPage(Model model){
         return "pages/member/mypage";
