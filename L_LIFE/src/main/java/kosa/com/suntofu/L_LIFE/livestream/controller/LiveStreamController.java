1a5058b80a50c371af5a6687e009318459f6d6a9
SeungJoo🍎 <seoungjoo01@gmail.com>
2023-08-27 22:51:55 +0900

    feat : 라이브 방송 시작 전 & 시작 후 화면 구현

---
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/livestream/controller/LiveStreamController.java
@@ -0,0 +1,22 @@
+package kosa.com.suntofu.L_LIFE.livestream.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/livestream")
+public class LiveStreamController {
+
+
+    /**
+     * 라이브 방송 화면 페이지 로드
+     * @return String
+     */
+    @GetMapping("")
+    public String loadLiveStreamPage(){
+        return "pages/livestream/livestream";
+    }
+}
