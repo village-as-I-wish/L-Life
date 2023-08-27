24ff69818da081bf746ced2dcb4ada65cace8a7d
SeungJoo🍎 <seoungjoo01@gmail.com>
2023-08-28 01:38:31 +0900

    feat : 프리미엄 메인 페이지 1차 구현

---
+++ L_LIFE/src/main/resources/static/js/premium/premium_main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+    // $('ul.tabs li').click(function(){
+    //     var tab_id = $(this).attr('data-tab');
+    //
+    //     $('ul.tabs li').removeClass('current');
+    //     $('.tab-content').removeClass('current');
+    //
+    //     $(this).addClass('current');
+    //     $("#"+tab_id).addClass('current');
+    // }),
+
+    $('.lf-pr-slide-wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        nextArrow:$('.next'),
+        prevArrow:$('.prev'),
+    });
+
+    $('.lf-pr-promotion-slide-wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        nextArrow:$('.lf-pr-promotion-slider .next'),
+        prevArrow:$('.lf-pr-promotion-slider .prev'),
+    });
+
+    $('.lf-pr-popular-slide-wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        nextArrow:$('.lf-pr-popular-slider .next'),
+        prevArrow:$('.lf-pr-popular-slider .prev'),
+    });
+
+
+    $('.lf-pr-slide-product-img').each(function(index) {
+        const $overlay = $(this).find('.img-overlay');
+        const $overlayText = $overlay.find('p');
+
+        $overlayText.text(`우드 & 화이트 감성 ${index + 1}`); // 원하는 텍스트 설정
+
+        // $(this).mouseenter(function() {
+        //     $overlay.css('opacity', 1);
+        // });
+        //
+        // $(this).mouseleave(function() {
+        //     $overlay.css('opacity', 0);
+        // });
+    });
+})
+
