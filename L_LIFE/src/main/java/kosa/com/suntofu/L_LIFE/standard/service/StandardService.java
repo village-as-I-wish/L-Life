dfcbf85bf0a179d65168bcf943b6e46deb28e4be
cherryJubilee <hyewon5026@gmail.com>
2023-08-28 17:20:32 +0900

    feat : [BE] 스탠다드 메인 - mapper 작성

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/service/StandardService.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/service/StandardService.java
@@ -1,20 +1,10 @@
 package kosa.com.suntofu.L_LIFE.standard.service;
 
-import kosa.com.suntofu.L_LIFE.standard.dao.StandardDAO;
 import kosa.com.suntofu.L_LIFE.standard.vo.StandardVO;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-public class StandardService implements StandardServiceImpl{
-
-    private final StandardDAO standardDAO;
-    public List<StandardVO> getAllStandard() {
-        return standardDAO.selectAllStandard();
-    }
-
+public interface StandardService {
+    List<StandardVO> getAllStandard();
 
 }
--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/service/StandardServiceImpl.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/standard/service/StandardServiceImpl.java
@@ -1,10 +1,20 @@
 package kosa.com.suntofu.L_LIFE.standard.service;
 
+import kosa.com.suntofu.L_LIFE.standard.dao.StandardDAO;
 import kosa.com.suntofu.L_LIFE.standard.vo.StandardVO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface StandardServiceImpl {
-    List<StandardVO> getAllStandard();
+@Service
+@RequiredArgsConstructor
+public class StandardServiceImpl implements StandardService{
+
+    private final StandardDAO standardDAO;
+    public List<StandardVO> getAllStandard() {
+        return standardDAO.selectAllStandard();
+    }
+
 
 }
