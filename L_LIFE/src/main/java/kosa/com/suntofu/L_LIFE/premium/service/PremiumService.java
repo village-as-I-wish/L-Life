96fab011ac9d28dad801f21c64fba23cb3077cfc
JOOWON <dev.joowon@gmail.com>
2023-08-29 17:46:51 +0900

    feat : [BE] 프리미엄 리스트 1차 완성

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/premium/service/PremiumService.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/premium/service/PremiumService.java
@@ -1,4 +1,12 @@
 package kosa.com.suntofu.L_LIFE.premium.service;
 
+import kosa.com.suntofu.L_LIFE.premium.vo.PaginationVo;
+import kosa.com.suntofu.L_LIFE.premium.vo.PremiumVo;
+
+import java.util.List;
+
 public interface PremiumService {
+
+    List<PremiumVo> selectPremiumProductList();
+    int selectProductCountPagination (PaginationVo paginationVo);
 }
