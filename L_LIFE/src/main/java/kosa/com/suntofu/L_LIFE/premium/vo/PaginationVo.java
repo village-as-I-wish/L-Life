eb2e086c01c9447baabb18f9113a5ec7c85847d7
JOOWON <dev.joowon@gmail.com>
2023-08-30 16:04:27 +0900

    chore : [BE] pagination css 수정 및 vo 변경

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/premium/vo/PaginationVo.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/premium/vo/PaginationVo.java
@@ -8,10 +8,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class PaginationVo {
-    private int fId; //리바트 가구 아이디
-    private String fImgUrl; // 가구 이미지 URL
-    private String fBrandName; // 가구 브랜드 이름
-    private String fName; // 가구 이름
+    private String lfImgMain; // 가구 이미지 URL
+    private String lfBrandName; // 가구 브랜드 이름
+    private String lfName; // 가구 이름
     private int lfPrPrice; // 프리미엄 월 구독료
     private int offset; // 오프셋 수
     private int page; // 페이지 수
