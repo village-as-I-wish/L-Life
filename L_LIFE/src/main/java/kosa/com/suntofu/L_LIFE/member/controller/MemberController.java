b7d6c8698634b64c98a38341a5166f02c5acd76c
sskong777 <sskong777@gmailcom>
2023-09-03 20:37:50 +0900

    fix login session

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/member/controller/MemberController.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/member/controller/MemberController.java
@@ -30,20 +30,21 @@ public class MemberController {
     public String kakaoLogin(@RequestParam String email, @RequestParam String name, @RequestParam String gender, @RequestParam String profile, HttpSession session){
         MemberVo memberVo = new MemberVo(1,name,gender,0,0,"",email,profile,"");
         MemberVo existingMember = memberService.insertOrSelectMember(memberVo);
-        int currentCoin = memberService.getCurrentCoin(existingMember.getMId());
-
-        int orderCount = memberService.getOrderCount(existingMember.getMId());
-        List<DeliveryStatusVo> deliveryStatusList = memberService.getDeliveryStatus(existingMember.getMId());
-        int deliveryReady = deliveryStatusList.get(0).getStatusCount();
-        int deliveryProgress = deliveryStatusList.get(1).getStatusCount();
-        int deliveryComplete = deliveryStatusList.get(2).getStatusCount();
+//        int currentCoin = memberService.getCurrentCoin(existingMember.getMId());
+//
+//        int orderCount = memberService.getOrderCount(existingMember.getMId());
+//        List<DeliveryStatusVo> deliveryStatusList = memberService.getDeliveryStatus(existingMember.getMId());
+//        int deliveryReady = deliveryStatusList.get(0).getStatusCount();
+//        int deliveryProgress = deliveryStatusList.get(1).getStatusCount();
+//        int deliveryComplete = deliveryStatusList.get(2).getStatusCount();
 
         // 세션 저장
         session.setAttribute("loggedInMemberInfo", existingMember);
-        session.setAttribute("currentCoin", currentCoin);
-        session.setAttribute("orderCount",orderCount);
-        session.setAttribute("deliveryProgress",deliveryProgress);        session.setAttribute("deliveryReady",deliveryReady);
-        session.setAttribute("deliveryComplete",deliveryComplete);
+//        session.setAttribute("currentCoin", currentCoin);
+//        session.setAttribute("orderCount",orderCount);
+//        session.setAttribute("deliveryProgress",deliveryProgress);
+//        session.setAttribute("deliveryReady",deliveryReady);
+//        session.setAttribute("deliveryComplete",deliveryComplete);
 
         return "pages/main/main";
     }
