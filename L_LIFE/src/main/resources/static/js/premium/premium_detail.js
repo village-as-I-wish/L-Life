def1a37d4abfe926ae60378c22aab35403810a97
SeungJoo🍎 <seoungjoo01@gmail.com>
2023-09-04 12:01:02 +0900

    feat : 프리미엄 화면 수정

---
+++ L_LIFE/src/main/resources/static/js/premium/premium_detail.js
@@ -0,0 +1,13 @@
+var optionId;
+$(document).ready(function(){
+
+    $('.lf-recommendation-slide-wrapper').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        nextArrow:$('.next'),
+        prevArrow:$('.prev'),
+    });
+
+});
