6ea41968f09904c3a0b186925e8063489002b6a9
SeungJoo🍎 <seoungjoo01@gmail.com>
2023-09-09 22:53:23 +0900

    feat : 패키지 메인 라인 레디스 캐싱 완료

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/constant/CacheKey.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/constant/CacheKey.java
@@ -6,5 +6,7 @@ import lombok.Getter;
 public class CacheKey {
 
     public static final String MDPICK_LLIFE_PACKAGES = "MDPICK_PACKAGES : ";
+    public static final String PROMOTION_LLIFE_PACKAGES = "PROMOTION_PACKAGES : ";
+    public static final String RECOMMENDATION_LLIFE_PACKAGES = "RECOMMENDATION_PACKAGES : ";
 
 }
