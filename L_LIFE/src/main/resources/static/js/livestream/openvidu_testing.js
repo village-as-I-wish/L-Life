2f6e0ff12f9872b157ce51ff3a657c81dcc0a08f
SeungJoo🍎 <seoungjoo01@gmail.com>
2023-09-11 16:50:14 +0900

    feat : 오픈비두 관리자 / 사용자 분리중

--- L_LIFE/src/main/resources/static/js/livestream/openvidu_testing.js
+++ L_LIFE/src/main/resources/static/js/livestream/openvidu_testing.js
@@ -7,70 +7,221 @@ $(document).ready(() => {
 
     var webComponent = document.querySelector('openvidu-webcomponent');
 
-    webComponent.addEventListener('onSessionCreated', (event) => {
-        var session = event.detail;
-    });
-    webComponent.addEventListener('onJoinButtonClicked', (event) => {});
-    webComponent.addEventListener('onToolbarLeaveButtonClicked', (event) => {});
-    webComponent.addEventListener('onToolbarCameraButtonClicked', (event) => {});
-    webComponent.addEventListener('onToolbarMicrophoneButtonClicked', (event) => {});
-    webComponent.addEventListener('onToolbarScreenshareButtonClicked', (event) => {});
-    webComponent.addEventListener('onToolbarParticipantsPanelButtonClicked', (event) => {});
-    webComponent.addEventListener('onToolbarChatPanelButtonClicked', (event) => {});
-    webComponent.addEventListener('onToolbarFullscreenButtonClicked', (event) => {});
-    webComponent.addEventListener('onParticipantCreated', (event) => {});
+    // webComponent.addEventListener('onSessionCreated', (event) => {
+    //     var session = event.detail;
+    // });
+    // webComponent.addEventListener('onJoinButtonClicked', (event) => {});
+    // webComponent.addEventListener('onToolbarLeaveButtonClicked', (event) => {});
+    // webComponent.addEventListener('onToolbarCameraButtonClicked', (event) => {});
+    // webComponent.addEventListener('onToolbarMicrophoneButtonClicked', (event) => {});
+    // webComponent.addEventListener('onToolbarScreenshareButtonClicked', (event) => {});
+    // webComponent.addEventListener('onToolbarParticipantsPanelButtonClicked', (event) => {});
+    // webComponent.addEventListener('onToolbarChatPanelButtonClicked', (event) => {});
+    // webComponent.addEventListener('onToolbarFullscreenButtonClicked', (event) => {});
+    // webComponent.addEventListener('onParticipantCreated', (event) => {});
+    // //
+    // $('.create-btn-txt').click(function() {
+    //     joinSession('admin')
+    // });
+    //
+    // $('.share-btn-txt').click(function() {
+    //     joinSession('user')
+    // });
+
+
 });
 
 
+function joinSession(isAdmin) {
+    var mySessionId = "dev-sskong2"
+    var myUserName = "sskong"
 
+    // Check if the user is an admin
 
-async function joinSession(isAdmin) {
-    var mySessionId = 'test';
-    var myUserName = 'sj';
+    // --- 1) Get an OpenVidu object ---
 
     OV = new OpenVidu();
+
+    // --- 2) Init a session ---
+
     session = OV.initSession();
 
-    session.on('streamCreated', event => {
-        var subscriber = session.subscribe(event.stream, 'video-container');
-        subscriber.on('videoElementCreated', event => {
-            const connection = subscriber.stream.connection;
-            appendUserData(event.element, connection);
+    // --- 3) Specify the actions when events take place in the session ---
 
-            // 관리자인 경우에만 자신의 화면을 추가
-            if (isAdmin == 1) {
-                event.element['muted'] = true;
-                initMainVideo(event.element, myUserName);
-            }
+    // On every new Stream received...
+    session.on('streamCreated', event => {
+
+        var subscriber = session.subscribe(event.stream, 'video-container');
+
+        // When the HTML video has been appended to DOM...
+        subscriber.on('videoElementCreated', event => {
+           //  initMainVideo(event.element, myUserName);
+           //
+           //  // Add a new <p> element for the user's nickname just below its video
+           // appendUserData(event.element, subscriber.stream.connection);
         });
     });
 
-    try {
-        var token = await getToken(mySessionId);
-        session.connect(token, { clientData: myUserName });
+    // On every Stream destroyed...
+    session.on('streamDestroyed', event => {
 
-        publisher = OV.initPublisher('video-container', {
-            audioSource: undefined,
-            videoSource: undefined,
-            publishAudio: true,
-            publishVideo: true,
-            resolution: '640x480',
-            frameRate: 30,
-            insertMode: 'PREPEND',
-            mirror: false
-        });
+        // Delete the HTML element with the user's nickname. HTML videos are automatically removed from DOM
+        removeUserData(event.stream.connection);
+    });
 
-        session.publish(publisher);
+    // On every asynchronous exception...
+    session.on('exception', (exception) => {
+        console.warn(exception);
+    });
 
-    } catch (error) {
-        console.log('There was an error:', error);
-    }
+    // --- 4) Connect to the session with a valid user token ---
+
+    // Get a token from the OpenVidu deployment
+    getToken(mySessionId).then(token => {
+
+        // First param is the token got from the OpenVidu deployment. Second param can be retrieved by every user on event
+        // 'streamCreated' (property Stream.connection.data), and will be appended to DOM as the user's nickname
+        session.connect(token, { clientData: myUserName })
+            .then(() => {
+
+                // --- 5) Set page layout for active call ---
+                //
+                // document.getElementById('session-title').innerText = mySessionId;
+                // document.getElementById('join').style.display = 'none';
+                // document.getElementById('session').style.display = 'block';
+
+                // --- 6) Get your own camera stream with the desired properties ---
+                if(isAdmin == 1){
+                    var publisher = OV.initPublisher('video-container', {
+                        audioSource: undefined, // The source of audio. If undefined default microphone
+                        videoSource: undefined, // The source of video. If undefined default webcam
+                        publishAudio: true,     // Whether you want to start publishing with your audio unmuted or not
+                        publishVideo: true,     // Whether you want to start publishing with your video enabled or not
+                        resolution: '640x480',  // The resolution of your video
+                        frameRate: 30,         // The frame rate of your video
+                        insertMode: 'PREPEND',   // How the video is inserted in the target element 'video-container'
+                        mirror: false          // Whether to mirror your local video or not
+                    });
+
+                    session.publish(publisher);
+
+                }else{
+                    console.log("testingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtestingtesting")
+
+                }
+                // var publisher = OV.initPublisher('video-container', {
+                //     audioSource: undefined, // The source of audio. If undefined default microphone
+                //     videoSource: undefined, // The source of video. If undefined default webcam
+                //     publishAudio: true,     // Whether you want to start publishing with your audio unmuted or not
+                //     publishVideo: true,     // Whether you want to start publishing with your video enabled or not
+                //     resolution: '640x480',  // The resolution of your video
+                //     frameRate: 30,         // The frame rate of your video
+                //     insertMode: 'PREPEND',   // How the video is inserted in the target element 'video-container'
+                //     mirror: false          // Whether to mirror your local video or not
+                // });
+
+                // --- 7) Specify the actions when events take place in our publisher ---
+
+                // When our HTML video has been added to DOM...
+                // publisher.on('videoElementCreated', function (event) {
+                //     // if (isAdmin !=1) {
+                //     //     event.element.style.display = 'none'; // Hide video for non-admin users
+                //     // }
+                //     if(isAdmin ==1 ){
+                //         initMainVideo(event.element, myUserName);
+                //         appendUserData(event.element, myUserName);
+                //         event.element['muted'] = true;
+                //     }else{
+                //
+                //     }
+                //     // initMainVideo(event.element, myUserName);
+                //     // appendUserData(event.element, myUserName);
+                //
+                // });
+
+                // --- 8) Publish your stream ---
+                // if(isAdmin ==1){
+                //     console.log("Admin 영역 : ", "testing")
+                //     session.publish(publisher);
+                //
+                // }
+
+            })
+            .catch(error => {
+                console.log('There was an error connecting to the session:', error.code, error.message);
+            });
+    });
 }
 
-// 관리자 여부를 확인하는 함수 (예: 어떤 조건에 따라서 관리자 여부 판단)
 
+//
+// async function joinSession(isAdmin) {
+//     var mySessionId = 'hong';
+//
+//     OV = new OpenVidu();
+//     session = OV.initSession();
+//
+//     try {
+//         var token = await getToken(mySessionId);
+//         session.connect(token, { clientData: isAdmin });
+//
+//         if (isAdmin === 'admin'){
+//             console.log("admin")
+//
+//             publisher = OV.initPublisher('video-container', {
+//                 audioSource: undefined,
+//                 videoSource: undefined,
+//                 publishAudio: true,
+//                 publishVideo: true,
+//                 resolution: '640x480',
+//                 frameRate: 30,
+//                 insertMode: 'PREPEND',
+//                 mirror: false
+//             });
+//
+//             publisher.on('videoElementCreated', function (event) {
+//                 // 사용자의 비디오를 표시하지 않도록 주석 처리
+//                 // document.querySelector('#video-container video').srcObject = event.element.srcObject;
+//                 event.element['muted'] = true;
+//             });
+//
+//             session.publish(publisher);
+//         }
+//
+//         else{
+//             console.log("user")
+//             session.on('streamCreated', event => {
+//
+//                 // Subscribe to the Stream to receive it. HTML video will be appended to element with 'video-container' id
+//                 var subscriber = session.subscribe(event.stream, 'video-container');
+//
+//                 // When the HTML video has been appended to DOM...
+//                 subscriber.on('videoElementCreated', event => {
+//
+//                     // Add a new <p> element for the user's nickname just below its video
+//                     appendUserData(event.element, subscriber.stream.connection);
+//                 });
+//             });
+//             session.on('streamCreated', event => {
+//                 console.log("stream connect data" + event.stream.connection.data)
+//                 console.log("!!!!!!!!!" + event.stream.connection.data)
+//                 if (event.stream.connection.data === 'admin') {
+//                     var subscriber = session.subscribe(event.stream, 'video-container');
+//                     subscriber.on('videoElementCreated', event => {
+//                         event.element['muted'] = true;
+//                     });
+//                 }
+//             });
+//         }
+//
+//
+//     } catch (error) {
+//         console.log('There was an error:', error);
+//     }
+// }
 
 function appendUserData(videoElement, connection) {
+    console.log("new ")
     var userData;
     var nodeId;
     if (typeof connection === "string") {
@@ -84,8 +235,10 @@ function appendUserData(videoElement, connection) {
     dataNode.className = "data-node";
     dataNode.id = "data-" + nodeId;
     dataNode.innerHTML = "<p>" + userData + "</p>";
-    // videoElement.parentNode.insertBefore(dataNode, videoElement.nextSibling);
-    // addClickListener(videoElement, userData);
+    videoElement.parentNode.insertBefore(dataNode, videoElement.nextSibling);
+    addClickListener(videoElement, userData);
+
+
 }
 
 var APPLICATION_SERVER_URL = "http://localhost:8080/l-life/";
