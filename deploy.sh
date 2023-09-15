6f8abcd1f9717d27f5d223027b91d2446671f84f
sskong777 <97655673+sskong777@users.noreply.github.com>
2023-09-15 14:36:48 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 DOCKER_APP_NAME="l_life"
 
 
-# backend 컨테이너 down
-echo "Backend down"
-docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
+# # backend 컨테이너 down
+# echo "Backend down"
+# docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
 
-# 이전 이미지 삭제
-docker rmi l_life:0.1
+# # 이전 이미지 삭제
+# docker rmi l_life:0.1
 
 echo "Backend up"
 docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d
