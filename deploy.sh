8f99b9c9984d06bc047b4927bef351bc44f76ea4
sskong777 <97655673+sskong777@users.noreply.github.com>
2023-09-15 10:52:16 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCKER_APP_NAME="l_life"
+
+
+# backend 컨테이너 down
+echo "Backend down"
+docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml down
+
+# 이전 이미지 삭제
+docker rmi l_life:0.1
+
+echo "Backend up"
+docker-compose -p ${DOCKER_APP_NAME} -f docker-compose.yml up -d
+
+sleep 5
+
+echo "Server On"
