c0dbad900d9a440b24221275c9ba0461cc430a5b
sskong777 <sskong777@gmail.com>
2023-09-18 17:41:52 +0900

    feat : [FE] Livart Book 만들기 페이지

---
+++ L_LIFE/src/main/resources/static/js/community/community_write.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    // Page.init();
+
+    $('.showroom-slide-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        nextArrow:$('.show-next'),
+        prevArrow:$('.show-prev'),
+    });
+
+
+    var pages = document.getElementsByClassName('page');
+    for(var i = 0; i < pages.length; i++)
+    {
+        var page = pages[i];
+        if (i % 2 === 0)
+        {
+            page.style.zIndex = (pages.length - i);
+        }
+    }
+
+    for(var i = 0; i < pages.length; i++)
+    {
+        //Or var page = pages[i];
+        pages[i].pageNum = i + 1;
+        pages[i].onclick=function()
+        {
+            if (this.pageNum % 2 === 0)
+            {
+                this.classList.remove('flipped');
+                this.previousElementSibling.classList.remove('flipped');
+            }
+            else
+            {
+                this.classList.add('flipped');
+                this.nextElementSibling.classList.add('flipped');
+            }
+        }
+    }
+
+
+})
