d242d57d0a52f8b7f112a7c84dc40eef711e6f4a
SeungJoo🍎 <seoungjoo01@gmail.com>
2023-09-24 17:40:23 +0900

    fix : 장바구니 버그 수정

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/subscription/vo/SubscriptionVo.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/subscription/vo/SubscriptionVo.java
@@ -1,12 +1,16 @@
 package kosa.com.suntofu.L_LIFE.subscription.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubscriptionVo {
 
     private Integer subscriptionId;
