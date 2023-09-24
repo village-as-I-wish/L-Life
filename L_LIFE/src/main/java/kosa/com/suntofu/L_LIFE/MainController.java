9b331994190576774b36e618cacd0fa6fd86c61a
JOOWON <dev.joowon@gmail.com>
2023-09-24 16:47:21 +0900

    fix : [BE] 이전 메인 삭제 및 새로운 메인 적용

--- L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/MainController.java
+++ L_LIFE/src/main/java/kosa/com/suntofu/L_LIFE/MainController.java
@@ -1,14 +1,8 @@
 package kosa.com.suntofu.L_LIFE;
 
-import kosa.com.suntofu.L_LIFE.member.SessionConst;
-import kosa.com.suntofu.L_LIFE.member.vo.MemberVo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -18,14 +12,9 @@ public class MainController {
         return "pages/main/livart_main";
     }
 
-    @GetMapping("/main2")
-    public String loadLlifeMainPage(Model model){
-        return "pages/main/main";
-    }
-
     @GetMapping("/main")
     public String loadLlifeNewMainPage(Model model){
-        return "pages/main/new_main";
+        return "pages/main/main";
     }
 
     @GetMapping("/mail")
